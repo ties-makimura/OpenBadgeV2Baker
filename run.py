a447c5b63e0f69e20de4cf5d43d49fde0f1c3ba1
Yukiharu YABUKI <yabuki@cccties.org>
2021-05-04 19:03:31 +0900

    Remove unused lines

--- run.py
+++ run.py
@@ -10,30 +10,13 @@ from BakeBadgeV2 import setup_logger
 from BakeBadgeV2 import ControlCenter
 
 
-
-
 # 各モジュールの最初のほうで、グローバルに宣言しておく
 logger = setup_logger(__name__)
 
-logger.info('hoge')
-logger.error('logfile:moge')
-
-# def _init_logger():
-#     logger = logging.getLogger('run')  #1
-#     logger.setLevel(logging.INFO)  #2
-#     handler = logging.StreamHandler(sys.stderr)  #3
-#     handler.setLevel(logging.INFO)  #4
-#     formatter = logging.Formatter(  
-#            '%(asctime)s:LEVEL:%(levelname)s:FILENAME:%(name)s:MODULENAME:%(module)s:MESSAGE:%(message)s') #5
-#     handler.setFormatter(formatter)  #6
-#     logger.addHandler(handler)  #7
-    
-
-
-# _init_logger()
-# _logger = logging.getLogger('run.py')
-# _logger.info('python script started in %s', os.getcwd())
+logger.info('start logging.info()')
+logger.error('start logging.error()')
 
 ControlCenter()
 
-
+logger.info('End logging.info()')
+logger.error('End logging.error()')
